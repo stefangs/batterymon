ee18d552e6ce7d43f59ae99f62747738b02da96a
Stefan G Strömberg <stefangs@nethome.nu>
2023-06-05 21:02:57 +0200

    Added session in report and saves in eeprom

--- batterymon/exteepromreporter.cpp
+++ batterymon/exteepromreporter.cpp
@@ -6,6 +6,7 @@
 #define MINUTE_IN_MS (60000)
 #define SAMPLE_TIME (60000)
 #define MARK (0xAABBCCD4)
+#define CURRENT_SESSION (16)
 #define REPORT_START (100)
 #define REPORT_SIZE (4096)
 #define SLOTS_PER_REPORT (REPORT_SIZE/sizeof(ExtSample))
@@ -37,7 +38,7 @@ ExtEEPromReporter::rawWriteEEProm(uint16_t address, const uint8_t* data, size_t 
 
 int 
 ExtEEPromReporter::writeEEProm(uint16_t address, const uint8_t* data, size_t len){
-  const uint8_t* dataToWrite = data;
+  uint8_t* dataToWrite = data;
   size_t lenRemaining = len;
   uint16_t nextAddress = address;
   int written = 0;
@@ -68,14 +69,14 @@ ExtEEPromReporter::readEEProm(uint16_t address, uint8_t* data, uint8_t len){
 }
 
 void 
-ExtEEPromReporter::begin(uint8_t address, TwoWire& i2c) {
+ExtEEPromReporter::begin(uint8_t address, TwoWire& i2c = Wire) {
   i2cAddress = address;
   twoWire = &i2c;
   readEEProm(0, (uint8_t *)&header, sizeof(ExtHeader));
   if (header.mark != MARK) {
     Serial.println("No mark, resetting eeprom");
     header.mark = MARK;
-    header.lastReport = 0;
+    header.lastReport = CURRENT_SESSION;
     writeEEProm(0, (uint8_t*)&header, sizeof(ExtHeader));
     slot = 0;
     extSample.unloadedVoltage = 0;
@@ -122,6 +123,8 @@ ExtEEPromReporter::reportStart(int startVoltage, int loadVoltage, int current) {
   slot = 0;
   nextSample = millis();
   reportSample(0, loadVoltage, startVoltage, 0, 0);
+  header.lastReport++;
+  writeEEProm(0, (uint8_t*)&header, sizeof(ExtHeader));
 }
 
 void
@@ -145,6 +148,22 @@ ExtEEPromReporter::reportEnd(int mAh) {
 
 void
 ExtEEPromReporter::reportWaiting() {
+}
+
+void
+ExtEEPromReporter::printSample(int session, long unsigned int timeMs, int loadedVoltage, int unloadedVoltage, int current, int mAh) {
+  Serial.print("> ");
+  Serial.print(session);
+  Serial.print(',');
+  Serial.print(timeMs / MINUTE_IN_MS);
+  Serial.print(',');
+  Serial.print(loadedVoltage);
+  Serial.print(',');
+  Serial.print(unloadedVoltage);
+  Serial.print(',');
+  Serial.print(current);
+  Serial.print(',');
+  Serial.println(mAh);
 }
 
 void 
@@ -174,7 +193,7 @@ ExtEEPromReporter::printReport() {
     minCurrent = min(minCurrent, current);
     mAMinutes += current;
     joule += (extSample.loadedVoltage * (double)current * 60.0)  / 1000000.0;
-    reporter.reportSample(time, extSample.loadedVoltage, extSample.unloadedVoltage, current, 0);
+    printSample(header.lastReport, time, extSample.loadedVoltage, extSample.unloadedVoltage, current, 0);
     time += SAMPLE_TIME;
     slot++;
     readSample(&extSample);
