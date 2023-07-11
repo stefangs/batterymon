c82cde19dd68310111be218eaf15c250dfa7b7cb
Stefan G Strömberg <stefangs@nethome.nu>
2023-07-11 21:22:18 +0200

    Added the plot command to batt

--- database/batt.py
+++ database/batt.py
@@ -1,6 +1,7 @@
 import psycopg2
 import fire
 from tabulate import tabulate
+import matplotlib.pyplot as plt
 
 class Batt(object):
   """Battery health app"""
@@ -12,7 +13,6 @@ class Batt(object):
     cursor = conn.cursor()
     return (conn, cursor)
 
-
   def list(self):
     """List all discharge sessions"""
     conn, cursor = self.__connectDatabase()
@@ -23,12 +23,32 @@ class Batt(object):
     headers = ["Session", "Voltage", "mAh", "Joule"]
     print(tabulate(rows, headers=headers))
 
-  def plot(self, session=0, battery=0):
-    """Plots the discharge graph, either for a session or all sessions for a battery"""
-    return 2
+  def plot(self, session=0):
+    """Plots the discharge graph for a session"""
+    conn, cur = self.__connectDatabase()
+    # Execute a SELECT statement to retrieve the data
+    cur.execute("SELECT minute, loaded, unloaded FROM samples where sess=%s;", (session,))
+    rows = cur.fetchall()
+    cur.close()
+    x = [row[0] for row in rows]
+    loaded = [row[1] for row in rows]
+    unloaded = [row[2] for row in rows]
+
+    # Create a scatter plot of the data
+    plt.plot(x, loaded, color='red', label='Loaded')
+    plt.plot(x, unloaded, color='blue', label='Unoaded')
+    plt.xlabel("Time (minutes)")
+    plt.ylabel("Voltage")
+    plt.title("Discharge voltage")
+    plt.ylim(0)
+    plt.legend()
+    plt.show()
+
+    # Close the database connection
+    conn.close()
   
   def read(self, battery=0, port='COM3'):
-    """Reads a discharge session from serial port and inserts in database"""
+    """NYI Reads a discharge session from serial port and inserts in database"""
     return 2
   
 if __name__ == '__main__':
